7db4b495b7f3bc7179d1a0a99767f9c1a922dfef
Koshevar Kirill <putumoney@gmail.com>
2020-05-31 16:55:00 +0300

    three

--- dbrepo/db_functions.go
+++ dbrepo/db_functions.go
@@ -4,21 +4,53 @@ import (
 	"log"
 )
 
-func GetGroups() GroupsResponse {
-	result, err := repo.Query("SELECT * from groups;")
+func onErrPanic(err error) {
 	if err != nil {
 		log.Panic(err)
 	}
-	defer result.Close()
+}
+func selecter(query string, args ...interface{}) {
+
+}
+
+//GetGroups позволяет получить список всех групп из БД
+func GetGroups() GroupsResponse {
+	resultGroups, err := repo.Query("SELECT * from groups;")
+	onErrPanic(err)
+	defer resultGroups.Close()
 
 	var Groups []Group
-
-	for result.Next() {
+	for resultGroups.Next() {
 		var group Group
-		err := result.Scan(&group.GroupID, &group.Title)
-		if err != nil {
-			log.Panic(err)
+		err := resultGroups.Scan(&group.GroupID, &group.Title)
+		onErrPanic(err)
+
+		resultTasks, err := repo.Query("SELECT * from tasks where group_id = $1", group.GroupID)
+		onErrPanic(err)
+
+		defer resultTasks.Close()
+		var Tasks []Task
+		for resultTasks.Next() {
+			var task Task
+			err := resultTasks.Scan(&task.TaskID, &task.Title, &task.GroupID)
+			onErrPanic(err)
+
+			resultTimeframes, err := repo.Query("SELECT * from time_frames where task_id = $1", task.TaskID)
+			onErrPanic(err)
+
+			var Timeframes []Timeframe
+			for resultTimeframes.Next() {
+				var timeframe Timeframe
+				err := resultTimeframes.Scan(&timeframe.TaskID, &timeframe.From, &timeframe.To)
+				onErrPanic(err)
+
+				Timeframes = append(Timeframes, timeframe)
+			}
+			task.Timeframes = Timeframes
+			Tasks = append(Tasks, task)
 		}
+
+		group.Tasks = Tasks
 		Groups = append(Groups, group)
 	}
 
@@ -27,6 +59,39 @@ func GetGroups() GroupsResponse {
 	return GroupsResp
 }
 
+//PostGroup позволяет запостить новую группу в БД
 func PostGroup() {
+
+}
+
+func PutGroup() {
+
+}
+
+func DeleteGroup() {
+
+}
+
+func GetTasks() {
+
+}
+
+func PostTask() {
+
+}
+
+func PutTask() {
+
+}
+
+func DeleteTask() {
+
+}
+
+func PostTimeFrame() {
+
+}
+
+func DeleteTimeFrame() {
 
 }
--- dbrepo/db_repo.go
+++ dbrepo/db_repo.go
@@ -11,10 +11,12 @@ import (
 
 var repo *sql.DB
 
+//DBSet установит объект *DB, через который можно будет взаимодействовать с базой данных
 func DBSet(db *sql.DB) {
 	repo = db
 }
 
+//DBInit открытие соединиение с БД. В ней же и устанавливается DBSet.
 func DBInit() *sql.DB {
 	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
 		dbUser, dbPassword, dbName)
